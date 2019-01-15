51160329b534c88778a8ac83cacc1f564a64621e
edgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-01-15 16:47:01 -0300

    add routes to the application and router-outlet in teh shell html file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = []
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
