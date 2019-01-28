6db8a3d24346a20d9d4238639d64f7cc7833d57e
edgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-01-28 11:44:27 -0300

    configure training.module to use training reducer

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -7,6 +7,8 @@ import { PastTrainingComponent } from './past-training/past-training.component';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { SharedModule } from '../shared/shared.module';
 import { TrainingRoutingModule } from './training-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './training.reducer';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,9 @@ import { TrainingRoutingModule } from './training-routing.module';
     SharedModule,
     AngularFirestoreModule,
     TrainingRoutingModule,
+    // we give to the training slice its reducer here
+    // because we lazy load this module
+    StoreModule.forFeature('training', reducer),
   ],
   entryComponents: [StopTrainingComponent],
 
