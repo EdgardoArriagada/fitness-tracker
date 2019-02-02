15506bc74e919b342134ef11c716809a39022a4a
edgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-02-02 11:42:29 -0300

    create auth.userEmail state

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,15 +1,29 @@
 import { Action } from '@ngrx/store';
+import { AuthDAta } from './auth-data.model';
 
-export const SET_AUTHENTICATED = '[Auth] Set Authenticated'
-export const SET_UNAUTHENTICATED = '[Auth] Set Unauthenticated'
+export const SET_AUTHENTICATED = '[Auth] SET_AUTHENTICATED'
+export const SET_UNAUTHENTICATED = '[Auth] SET_UNAUTHENTICATED'
+export const SET_EMAIL = '[Auth] SET_EMAIL'
 
 export class SetAuthenticated implements Action {
   readonly type = SET_AUTHENTICATED;
-  constructor(public userID: string,) {}
+  constructor(
+    public userID: string,
+    public userEmail: AuthDAta['email'],
+  ) { }
 }
 
 export class SetUnauthenticated implements Action {
   readonly type = SET_UNAUTHENTICATED;
 }
 
-export type AuthActions = SetAuthenticated | SetUnauthenticated
+export class SetEmail implements Action {
+  readonly type = SET_EMAIL;
+  constructor(
+    public userEmail: AuthDAta['email'],
+  ) { }
+}
+
+export type AuthActions = SetAuthenticated
+| SetUnauthenticated
+| SetEmail
